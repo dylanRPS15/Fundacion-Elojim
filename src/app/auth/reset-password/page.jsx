d0c1c426489c6b3ad88128d715bd6d11135f18f2
dylanRPS15 <u20202192371@usco.edu.co>
2025-11-01 16:27:00 -0500

    fix: correct use client order in reset-password page

--- src/app/auth/reset-password/page.jsx
+++ src/app/auth/reset-password/page.jsx
@@ -1,7 +1,7 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 60;
-
-"use client";
 
 import { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
