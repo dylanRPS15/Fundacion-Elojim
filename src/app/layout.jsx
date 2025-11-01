a9bba67cb5e7b22d699e6dabe001056398350d0f
dylanRPS15 <u20202192371@usco.edu.co>
2025-11-01 12:18:02 -0500

    fix:  cambio en layout para dimanizar paginas

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
